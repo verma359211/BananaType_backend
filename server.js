96f96af29bdfc893391082c92efdfcb64e927a35
verma359211 <vermachandan2912003@gmail.com>
2025-03-07 23:01:10 +0530

    commit

--- server.js
+++ server.js
@@ -155,6 +155,6 @@ io.on("connection", (socket) => {
 });
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
